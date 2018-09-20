390e7c8fff9ec858991295af579bf6a0328bfd52
Simon O'Brien <simon@the-obriens.net>
2018-09-20 22:47:19 +0100

    Restored cron settings and setup keybindings

--- tasks/system/setup-keybindings.sh
+++ tasks/system/setup-keybindings.sh
@@ -4,3 +4,5 @@ PID=$(pgrep -f 'gnome-session' | head -n1)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
 gsettings set org.cinnamon.desktop.keybindings.wm maximize "['<Alt><Super>Up']"
+gsettings set org.cinnamon.desktop.keybindings.wm unmaximize "['<Alt><Super>Down']"
+gsettings set org.cinnamon.desktop.keybindings.wm minimize "['<Shift><Alt><Super>Down']"
