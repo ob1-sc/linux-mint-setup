d558f57e91fafcf6355987da32beef8c21fc7dd4
Simon O'Brien <simon@the-obriens.net>
2019-07-28 12:54:30 +0100

    Disabled plank setup

--- tasks/desktop/setup-plank.sh
+++ tasks/desktop/setup-plank.sh
@@ -4,10 +4,10 @@ PID=$(pgrep -f 'gnome-session' | head -n1)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
 # setup plank preferences
-dconf write /net/launchpad/plank/docks/dock1/alignment 'center'
-dconf write /net/launchpad/plank/docks/dock1/theme 'Transparent'
-dconf write /net/launchpad/plank/docks/dock1/position 'left'
-dconf write /net/launchpad/plank/docks/dock1/hide-delay 300
+#dconf write /net/launchpad/plank/docks/dock1/alignment 'center'
+#dconf write /net/launchpad/plank/docks/dock1/theme 'Transparent'
+#dconf write /net/launchpad/plank/docks/dock1/position 'left'
+#dconf write /net/launchpad/plank/docks/dock1/hide-delay 300
 
 # setup plank launchers
 # rm -rf /home/siobrien/.config/plank/dock1/*
