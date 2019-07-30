a13208e95b4987a4634b7f447186e3aefc95acec
Simon O'Brien <simon@the-obriens.net>
2019-07-30 10:08:43 +0100

    Removed vim plugin for vscode

--- tasks/apps/vscode/install-extensions.sh
+++ tasks/apps/vscode/install-extensions.sh
@@ -4,4 +4,4 @@ code --install-extension vscoss.vscode-ansible
 code --install-extension emmanuelbeziat.vscode-great-icons
 code --install-extension marvinhagemeister.theme-afterglow-remastered
 code --install-extension mauve.terraform
-code --install-extension vscodevim.vim
+#code --install-extension vscodevim.vim
