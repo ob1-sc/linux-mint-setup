9cc20619129317d7e8f4e69ea3b7d84352fd25d2
Simon O'Brien <simon@the-obriens.net>
2019-07-30 09:30:46 +0100

    Updated vscode setup and plugins

--- tasks/apps/vscode/install-extensions.sh
+++ tasks/apps/vscode/install-extensions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 code --install-extension vscoss.vscode-ansible
-code --install-extension robertohuertasm.vscode-icons
+code --install-extension emmanuelbeziat.vscode-great-icons
 code --install-extension marvinhagemeister.theme-afterglow-remastered
+code --install-extension mauve.terraform
+code --install-extension vscodevim.vim
