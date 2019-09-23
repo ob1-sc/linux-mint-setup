49f05518335bb06014c287fec84832f82cf143d3
Simon O'Brien <simon@the-obriens.net>
2019-09-23 20:07:13 +0100

    Refactoed from tasks to roles

--- tasks/system/setup-bash-aliases.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASH_ALIASES_FILE_NAME=~/.bash_aliases
-
-if ! grep -Fq "alias code" "$BASH_ALIASES_FILE_NAME"; then
-    echo 'alias code="GTK_IM_MODULE=xim code"'  >> ~/.bash_aliases
-fi
